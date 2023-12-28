af567ad71c80a20f2231387b5b411b988899fe02
maxange-developer <massiangelone73@gmail.com>
2023-12-28 18:03:58 +0100

    fine progetto

--- src/js/index.js
+++ src/js/index.js
@@ -1,138 +1,191 @@
-import '../css/style.css'
+import "../css/style.css";
+import axios from "axios";
 
 const body = document.body;
 
 // Creazione elemento div per lo sfondo
 
-const background =document.createElement('div');
+const background = document.createElement("div");
 body.appendChild(background);
-background.setAttribute('class', 'background')
+background.setAttribute("class", "background");
 
 // Creazione elemento section della schermata iniziale
 
-const section = document.createElement('section');
+const section = document.createElement("section");
 body.appendChild(section);
-section.setAttribute('class', 'header')
+section.setAttribute("class", "header");
 
 // Funzione di creazione elementi HTML
 
 function create(nome, tag, parent, id, text, classSelector, placeholder) {
-    nome = document.createElement(tag);
-    parent.appendChild(nome);
-    nome.setAttribute('id', `${id}`);
-    nome.textContent = `${text}`;
-    if (classSelector) {
-        nome.setAttribute('class', `${classSelector}`)
-    }
-    else if (placeholder) {
-        nome.setAttribute('placeholder', `${placeholder}`);
-        nome.setAttribute('type', 'text')
-    } return nome;
+  nome = document.createElement(tag);
+  parent.appendChild(nome);
+  nome.setAttribute("id", `${id}`);
+  nome.textContent = `${text}`;
+  if (classSelector) {
+    nome.setAttribute("class", `${classSelector}`);
+  } else if (placeholder) {
+    nome.setAttribute("placeholder", `${placeholder}`);
+    nome.setAttribute("type", "text");
+  }
+  return nome;
 }
 
 // Creazione elementi della schermata iniziale
 
-const title = create('title', 'h1', section, 'title', 'LifeStyle');
-const input = create('input', 'input', section, '','','','please enter a city name');
-const button = create('button', 'button', section, '', 'search');
+const title = create("title", "h1", section, "title", "LifeStyle");
+const input = create(
+  "input",
+  "input",
+  section,
+  "",
+  "",
+  "",
+  "please enter a city name"
+);
+const button = create("button", "button", section, "", "search");
 
-let city;
+// Funzione di Ricerca
 
-let cityName = function (name) {
-    name = name.toLowerCase();
+async function performSearch() {
+  let cityName = function (name) {
     name = name.trim();
     name = name.replaceAll(" ", "-");
     return name;
-};
+  };
 
-const getData = async function () {
-    const getScore = await fetch(
-        `https://api.teleport.org/api/urban_areas/slug:${city}/scores/`
+  let city = cityName(input.value);
+
+  if (city.length === 0) {
+    alert("Please, enter a city name.");
+    return;
+  }
+
+  try {
+    const data = await fetchCityData(city);
+    displayResults(data);
+  } catch (error) {
+    console.error(error);
+    alert("City not found.");
+  }
+}
+
+// Chiamate API
+
+async function fetchCityData(city) {
+  try {
+    const response = await axios.get(
+      `https://api.teleport.org/api/cities/?search=${city}`
     );
+    const data = response.data;
 
-    const dataScore = await getScore.json();
+    if (data.count === 0) {
+      throw new Error("City not found.");
+    }
 
-    const getImage = await fetch(
-        `https://api.teleport.org/api/urban_areas/slug:${city}/images/`
+    const cityId =
+      data._embedded["city:search-results"][0]._links["city:item"].href;
+    const cityResponse = await axios.get(cityId);
+    const cityData = cityResponse.data;
+
+    // Score città
+    const urbanAreaId = cityData._links["city:urban_area"].href;
+    const urbanAreaScoresResponse = await axios.get(`${urbanAreaId}scores/`);
+    const urbanAreaScoresData = urbanAreaScoresResponse.data;
+
+    //Immagine città
+    const urbanAreaSlug = cityData._links["city:urban_area"].href
+      .split("/")
+      .slice(-2)[0];
+    const urbanAreaImagesResponse = await axios.get(
+      `https://api.teleport.org/api/urban_areas/${urbanAreaSlug}/images/`
     );
+    const urbanAreaImagesData = urbanAreaImagesResponse.data;
 
-    const dataImage = await getImage.json()
-
-    // Funzione cambiamento dello sfondo
-    const changeBackground = () => {
-        const bg = document.querySelector(".background");
-        bg.remove();
-        body.style.backgroundImage = "url(https://img.freepik.com/premium-photo/free-plain-sky-image_906250-81.jpg?size=626&ext=jpg&ga=GA1.1.1294237815.1669366217&semt=ais)";
-        body.style.backgroundRepeat = "no-repeat";
-        body.style.width = "100%";
-        body.style.height = "100%";
-        body.style.backgroundSize = "cover";
+    return {
+      teleport_cityName:
+        data._embedded["city:search-results"][0].matching_full_name,
+      teleport_city_score: urbanAreaScoresData.teleport_city_score,
+      summary: urbanAreaScoresData.summary,
+      categories: urbanAreaScoresData.categories,
+      imageUrl: urbanAreaImagesData.photos[0].image.web,
     };
+  } catch (error) {
+    throw new Error("error during api call");
+  }
+}
 
-    if (getScore.status != 404) {
+// Schermata città
 
-        // Rimozione schermata iniziale e cambio di sfondo
+async function displayResults(data) {
+  // Funzione cambiamento dello sfondo
+  const changeBackground = () => {
+    const bg = document.querySelector(".background");
+    bg.remove();
+    body.style.backgroundImage =
+      "url(https://img.freepik.com/premium-photo/free-plain-sky-image_906250-81.jpg?size=626&ext=jpg&ga=GA1.1.1294237815.1669366217&semt=ais)";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.width = "100%";
+    body.style.height = "100%";
+    body.style.backgroundSize = "cover";
+  };
+  section.innerHTML = section.remove();
+  changeBackground();
 
-        section.innerHTML = section.remove();
-        changeBackground();
+  //  Creazione nuovi elementi HTML
 
-        //  Creazione nuovi elementi HTML
+  const database = create("section", "section", body, "", "", "database");
+  const cityHeader = create(
+    "city-header",
+    "div",
+    database,
+    "",
+    "",
+    "city-header"
+  );
+  const heading = create("heading", "div", database, "", "", "heading");
 
-        const database = create('section', 'section', body, '', '', 'database');
-        const cityHeader = create('city-header', 'div', database, '', '', 'city-header');
-        const heading = create('heading', 'div', database, '', '', 'heading');
-        
-        const box = create('box', 'div', heading, '', '', 'box');
-        const image = create('image', 'div', box, '', '', 'image');
-        const summary = create('summary', 'div', box, '', '', 'summary');
+  const box = create("box", "div", heading, "", "", "box");
+  const image = create("image", "div", box, "", "", "image");
+  const summary = create("summary", "div", box, "", "", "summary");
 
-        const score = create('score', 'div', database, '', '', 'score');
-        const textScore = create('text-score', 'p', score, '', '', 'text-score');
-        const cityScore = create('city-score', 'p', score, '', '', 'city-score');
-        const category = create('category', 'p', score, '', '', 'category');
+  const score = create("score", "div", database, "", "", "score");
+  const textScore = create("text-score", "p", score, "", "", "text-score");
+  const cityScore = create("city-score", "p", score, "", "", "city-score");
+  const category = create("category", "p", score, "", "", "category");
 
-        cityHeader.innerHTML = `<h2>${city}</h2>`;
-        image.innerHTML = `<img src="${dataImage.photos[0].image.web}" alt=${city}>`;
+  // Dati API -> HTML
 
-        summary.innerHTML = `<h2><p>${dataScore.summary}</p></h2>`;
-        textScore.innerHTML = "CITY SCORE:";
-        cityScore.innerHTML = dataScore.teleport_city_score.toFixed(2);
-        dataScore.categories.forEach((x) => {
-            category.insertAdjacentHTML(
-                "afterbegin",
-                `<p>${x.name}: ${x.score_out_of_10.toFixed(1)}<p>`
-                );
+  cityHeader.innerHTML = `<h2>${data.teleport_cityName}</h2>`;
+  image.innerHTML = `<img src="${data.imageUrl}" alt=${data.teleport_cityName}>`;
 
-                let ps = document.getElementsByTagName('p');
-                for (let el of ps) {
-                cleaner(el);
-            };
+  summary.innerHTML = `<h2><p>${data.summary}</p></h2>`;
+  textScore.innerHTML = "CITY SCORE:";
+  cityScore.innerHTML = data.teleport_city_score.toFixed(2);
+  data.categories.forEach((x) => {
+    category.insertAdjacentHTML(
+      "afterbegin",
+      `<p>${x.name}: ${x.score_out_of_10.toFixed(1)}<p>`
+    );
 
-            function cleaner(el) {
-                if (el.innerHTML == '&nbsp;' || el.innerHTML == '') {
-                    el.parentNode.removeChild(el);
-                }
-            };
-        });
-    };  
-};
-
-const errorEmpty = () => {
-    if (!input.value) {
-      alert('Enter a city, please!');
+    // Pulizia paragrafi vuoti
+    let ps = document.getElementsByTagName("p");
+    for (let el of ps) {
+      cleaner(el);
     }
-};
-  
-input.addEventListener("keydown", function (enterkey) {
-    if (enterkey.key === "Enter") {
-      city = cityName(input.value);
-      getData();
-      errorEmpty();
+
+    function cleaner(el) {
+      if (el.innerHTML == "&nbsp;" || el.innerHTML == "") {
+        el.parentNode.removeChild(el);
+      }
     }
+  });
+}
+
+input.addEventListener("keydown", (enterkey) => {
+  if (enterkey.key === "Enter") {
+    performSearch();
+  }
 });
-  
-button.addEventListener("click", function () {
-    city = cityName(input.value);
-    getData();
-    errorEmpty();
-});
+
+button.addEventListener("click", () => performSearch());
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,49 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackFavicons = require('webpack-favicons');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackFavicons = require("webpack-favicons");
 
 module.exports = {
-    entry: {
-        index: './src/js/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test:/\.(png|svg|jpe?g)$/i,
-                type: 'asset/resource'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new WebpackFavicons({
-            src: './src/img/favicon.png',
-            path: 'img',
-            icons: {
-                favicons: true
-            }
-        })
+  entry: {
+    index: "./src/js/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/i,
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpe?g)$/i,
+        type: "asset/resource",
+      },
     ],
-    devServer: {
-        port: 5000,
-        open: true,
-        static: path.resolve(__dirname, 'dist')
-    },
-    mode: 'production'
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new WebpackFavicons({
+      src: "./src/img/favicon.png",
+      path: "img",
+      icons: {
+        favicons: true,
+      },
+    }),
+  ],
+  devServer: {
+    port: 5000,
+    open: true,
+    static: path.resolve(__dirname, "dist"),
+  },
+  mode: "production",
+};
